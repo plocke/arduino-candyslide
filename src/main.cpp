addbc884b34ad804043f31891de37e648d37b272
Peter Locke <peter@kiind.me>
2022-10-17 22:15:47 -0700

    second servo basics added.

--- src/main.cpp
+++ src/main.cpp
@@ -8,7 +8,7 @@
 
 
 Servo myservo; // create servo object to control a servo
-//Servo myTopservo; // create servo object to control a servo
+Servo myTopservo; // create servo object to control a servo
 
 bool ALLOW_SERIAL = false;
 int candyButton = 2;
@@ -16,7 +16,7 @@ int resetButton = A5;
 int slideLightPin = 7;
 int candyLightPin = 8;
 int servoPin = 9;
-//int topServoPin = 10;
+int topServoPin = 13;
 LiquidCrystal lcd(12, 11, 10, 6, 5, 4, 3);
 eeprom_config_struct eepromsave;
 
@@ -26,6 +26,7 @@ unsigned long lastPulseTime = 0;
 unsigned long lastCandyButtonStateChange = 0;
 int candyGiven = 0;
 
+const int SERVO_CANDY_CYCLE_DELAY = 500;
 const long candyLightPulseLengthMs = 1000;
 const long activateLightsPulseLengthMs = 75;
 const int debounceTimeMillis = 100;
@@ -48,21 +49,21 @@ void setup()
   EEPROM_readAnything(EEPROM_SAVELOCATION, eepromsave);
   candyGiven = eepromsave.candylifetime;
 
-
   myservo.attach(servoPin); // attaches the servo on pin 9 to the servo object
-  // myTopservo.attach(topServoPin);
-  // myTopservo.write(180);
+  myTopservo.attach(topServoPin);
+  myTopservo.write(0);
   pinMode(candyButton, INPUT_PULLUP);
   pinMode(resetButton, INPUT_PULLUP);
   pinMode(candyLightPin, OUTPUT);
   pinMode(slideLightPin, OUTPUT);
   digitalWrite(slideLightPin, HIGH);
   myservo.write(0);  // starting position;
+  myTopservo.write(0);
   //myservo.detach();
   lcd.begin(20,4);              // columns, rows.  use 16,2 for a 16x2 LCD, etc.
   lcd.clear();                  // start with a blank screen
   setFirstLine("CANDY GIVEN: "+String(candyGiven), lcd);    // change text to whatever you like. keep it clean!
-  setSecondLine("---", lcd);
+  setSecondLine("-SLIDE v3--", lcd);
   setThirdLine("Press Foot Button", lcd);
   setFourthLine("For Candy!", lcd);
 
@@ -113,8 +114,9 @@ void loop()
     setSecondLine("! DISPENSING CANDY !", lcd);
     setThirdLine("Trick-or-Treat # "+String(candyGiven), lcd);
     setFourthLine("********************", lcd);
-    myservo.write(0);
     myservo.write(180);
+    myTopservo.write(180);
+
     for (int i = 0; i < flashCountOnCandy; i++)
     {
       digitalWrite(candyLightPin, HIGH);
@@ -129,8 +131,11 @@ void loop()
       digitalWrite(slideLightPin, HIGH);
 
 
-    delay(1000- (activateLightsPulseLengthMs*2*flashCountOnCandy));
+    //delay(1000- (activateLightsPulseLengthMs*2*flashCountOnCandy));
+    delay(SERVO_CANDY_CYCLE_DELAY);
     myservo.write(0);
+    myTopservo.write(0);
+    
     for (int i = 0; i < flashCountOnCandy; i++)
     {
       digitalWrite(slideLightPin, HIGH);
@@ -162,7 +167,6 @@ void loop()
     EEPROM_writeAnything(EEPROM_SAVELOCATION, eepromsave);
     lcd.clear();
     setFirstLine("Reset candy count", lcd);    // change text to whatever you like. keep it clean!
-
   }
   
   //myservo.writeMicroseconds(0);                // sets the servo position according to the scaled value
